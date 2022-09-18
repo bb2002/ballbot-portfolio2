7415e8ee410c153b7162bfbe0c876fda25852f2f
Ballbot <5252bb@daum.net>
2022-09-18 18:35:35 +0900

    [ADD] header 추가

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Portfolio;
---
+++ src/styles/common/Header.css.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  margin: 40px 0 40px 0;
+  display: flex;
+  justify-content: center;
+`
+
+export const HeaderItem = styled.a`
+  color: white;
+  width: 200px;
+  font-family: 'IBM Plex Sans KR', sans-serif;
+  font-weight: bold;
+  text-decoration: none;
+  font-size: 1.5rem;
+`

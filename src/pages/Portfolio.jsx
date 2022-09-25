3ee90af7957caec4dd52ee8d195ab0a6d915e120
Ballbot <5252bb@daum.net>
2022-09-25 18:49:19 +0900

    [FIX] 모바일 뷰 대응

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,13 +6,25 @@ import Tech from "./subpages/Tech";
 import Projects from "./subpages/Projects";
 import { useEffect } from "react";
 
+function getOffset(el) {
+  const rect = el.getBoundingClientRect();
+  return {
+    left: rect.left + window.scrollX,
+    top: rect.top + window.scrollY
+  };
+}
+
 const Portfolio = () => {
   useEffect(() => {
     const tab = window.location.href.split('#')[1];
 
     if (tab === 'projects') {
       // 프로젝트에서 뒤로 온 경우, PROJECT 화면으로 이동
-      document.body.scrollTo(0, 4680.4);
+      const offset = getOffset(document.getElementById('projects')).top;
+
+      if (offset > 100) {
+        document.body.scrollTo(0, offset);
+      }
     }
   }, [])
 
